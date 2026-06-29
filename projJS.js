68950aff19c1da2dd04359da91d0960034aeeaff
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-06-29 09:59:41 +0300

    Added random word list and tested it to choose a random word successfully

--- projJS.js
+++ projJS.js
@@ -2,3 +2,11 @@ const wordList =
 [
     'APPLE', 'PLANT', 'BRAIN', 'HUMID'
 ]
+
+const randIndex = Math.floor(Math.random() * wordList.length)
+
+const randWord = wordList[randIndex]
+
+
+console.log(randWord)
+
