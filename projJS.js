939bb86285682d7a5fd01eca8964828d52c7ac00
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-06-29 14:26:29 +0300

    Made function to listen to keyboard inputs and place them in the first row of tiles successfully

--- projJS.js
+++ projJS.js
@@ -6,10 +6,28 @@ const wordList =
 const randIndex = Math.floor(Math.random() * wordList.length)
 
 let answer = wordList[randIndex]
+// made a word list to choose from, then randIndex randomizes through the array's index, math.floor is to erase decimals, then the answer is chosen with the answer variable, print out the word from the array of this [randIndex] randomized index
+
 
 let currentGuess = ''
 let currentRow = 0
 let gameOver = false
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const tiles = document.querySelectorAll('.tile')
 
-console.log(gameOver)
+document.addEventListener('keydown', function(event){
+   const key = event.key.toUpperCase() 
+    if(alphabet.includes(key) && currentGuess.length < 5 && gameOver === false){
+       const checkTiles = currentRow * 5 + currentGuess.length
+       tiles[checkTiles].textContent = key
+       currentGuess = currentGuess + key
+       console.log(currentGuess)
+        
+    }
+})
+
+
+
+
+
 
