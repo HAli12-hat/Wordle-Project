0d610597f9a8251c9defcfb4515d05ac5d06a2a1
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-06-30 12:45:43 +0300

    Finally made it work so that it checks letters properly, if the user guesses a word with a letter used more than once, if that letter exists only once in answer, I made it so that it checks for the first letter first and the other one gets grayed out

--- projJS.js
+++ projJS.js
@@ -1,6 +1,6 @@
 const wordList = 
 [
-    'APPLE', 'PLANT', 'BRAIN', 'HUMID'
+    'PLANT'
 ]
 
 const randIndex = Math.floor(Math.random() * wordList.length)
@@ -39,6 +39,10 @@ document.addEventListener('keydown', function(event){
 
 // The second if statement checks if the player tries to delete letters he entered by listening to BACKSPACE, and the current guess has to not be empty. Take the same equation from before that adds the listened key, and this time it is used to find the tile that has the current guess's last letter, and clears that tile, as well as deleting the letter.
 
+// for the checkGuess() function, a const is declared that takes a copy of the answer and splits its string into an array where each letter is an array item, this is done so later it gets checked with whatever the user types in. It loops twice, first loop: loop through guess indices and check if each index matches answer indices, if yes, color green. Then, mark each correct letter empty in the copy array so it is never used again.
+
+// Second loop: check if guessed letters aren't the same as the answer's, then create a constant that goes through the array of the answer, and look for an unused matching letter. IndexOf() returns -1 if the item isnt there, but if it is there (returns anything but -1), then color it yellow then replace the copy of the answer with an empty string so it is never used again. Else, color it gray.
+
 function addLetter(pressedKey){
       
        const tileIndex = currentRow * 5 + currentGuess.length
@@ -59,21 +63,39 @@ function addLetter(pressedKey){
 
 
     function checkGuess(){
+           const remainingLetters = answer.split('')
+        //    check if correct letter, if yes, color tile green 
         for (let i = 0; i < currentGuess.length; i++) {
         if (currentGuess[i] === answer[i]) {
             const tileIndex = currentRow * 5 + i
             tiles[tileIndex].classList.add('correct-color')
+            // mark answer as used
+            remainingLetters[i] = ''
         }
-        else if(answer.includes(currentGuess[i]) !== true){
+        }
+        // second loop to check for yellow and gray
+        for (let i = 0; i < currentGuess.length; i++){
             const tileIndex = currentRow * 5 + i
-            tiles[tileIndex].classList.add('wrong-color')
+
+            if(currentGuess[i] !== answer[i]){
+                const foundIndex = remainingLetters.indexOf(currentGuess[i])
+                if(foundIndex !== -1){
+                    const tileIndex = currentRow * 5 + i
+                    tiles[tileIndex].classList.add('almost-color')
+                    remainingLetters[foundIndex] = ''
+                }
+                else{
+                    const tileIndex = currentRow * 5 + i
+                    tiles[tileIndex].classList.add('wrong-color')
+            }
         }
-        else{
-            const tileIndex = currentRow * 5 + i
-            tiles[tileIndex].classList.add('almost-color')
-        }
+             
+             
+        
+       
     }
-    }
+}
+    
     function submitGuess(){
         checkGuess()
         console.log(currentGuess)
@@ -83,8 +105,14 @@ function addLetter(pressedKey){
         currentGuess = ''
     }
 
+
     
+
     
+     
+            // const tileIndex = currentRow * 5 + i
+            // tiles[tileIndex].classList.add('almost-color')
+        
 
     
     // if(alphabet.includes(key) && currentGuess.length < 5 && gameOver === false){
